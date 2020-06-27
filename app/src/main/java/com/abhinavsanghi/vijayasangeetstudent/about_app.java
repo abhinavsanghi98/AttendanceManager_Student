b09c00f62456c7d8a3df4ab41d3f5fd87b759a16
abhinavsanghi98 <sanghi.aabhu1@gmail.com>
2020-06-28 01:33:26 +0530

    Initial Commit

---
+++ app/src/main/java/com/abhinavsanghi/vijayasangeetstudent/about_app.java
@@ -0,0 +1,16 @@
+package com.abhinavsanghi.vijayasangeetstudent;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+//import com.developerdesk9.attendanceregister.R;
+
+public class about_app extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_app);
+
+    }
+}
